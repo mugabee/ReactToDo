f82bf554f711f53fa2c23c1d76f73dfa7fbc5731
mugabe richard <mugabe.richard.37@gmail.com>
2021-10-31 07:36:34 +0200

    creating the items list length

--- src/components/List.js
+++ src/components/List.js
@@ -6,19 +6,29 @@ const List = ({ items }) => {
         <article >
             <ul className="bg-gray-700 mx-5  rounded-lg mt-10">
                 {items.map(({id, title }) => (
-                    <ul className="flex border-b px-5 border-gray-600 justify-between">
+                    <ul className="flex border-b px-8 border-gray-600 justify-between">
                         <li key={id}
                         className="todo-list text-white  py-3 py-2 tracking-wider  "
                         >
                             {title}
                         </li>
-                        <button>
-                            <IoClose />
+                        <button className="text-xl">
+                            <IoClose className="text-red-500" />
                         </button>
 
 
                     </ul>
                 ))}
+                <ul>
+                    <li>
+                        <p className="text-sm text-gray-400">
+                            {items.length} Total ToDo
+                        </p>
+                    </li>
+                    <li>
+                        <button>Clear List</button>
+                    </li>
+                </ul>
             </ul>
         </article>
         </>
