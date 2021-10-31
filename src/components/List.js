7f0aa1f43b90d2ca7eeb7bcc1fa118d9488cd8fd
mugabe richard <mugabe.richard.37@gmail.com>
2021-10-31 07:19:48 +0200

    Randering the to do items to our main screen

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,23 @@
+import { IoClose } from  "react-icons/io5"
+
 const List = ({ items }) => {
     return (
         <>
-        <article>
-            <ul>
+        <article >
+            <ul className="bg-gray-700 mx-5  rounded-lg mt-10">
                 {items.map(({id, title }) => (
-                    <li key={id}>{title}</li>
+                    <ul className="flex border-b px-5 border-gray-600 justify-between">
+                        <li key={id}
+                        className="todo-list text-white  py-3 py-2 tracking-wider  "
+                        >
+                            {title}
+                        </li>
+                        <button>
+                            <IoClose />
+                        </button>
+
+
+                    </ul>
                 ))}
             </ul>
         </article>
