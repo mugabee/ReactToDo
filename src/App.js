4141d75cc92e7cdbe6454ce427bfb57d10fc865c
mugabe richard <mugabe.richard.37@gmail.com>
2021-10-31 05:40:12 +0200

    Styling the To DO  input

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import Logo from "./components/Logo"
 import Hero from "./components/Hero"
 
 const App = () => {
-  return (
-    <>
-      <main>
-        <Logo />
-        <Hero />
+    return ( 
+        <>       
+            <main>        
+                <Logo />        
+                <Hero />  
 
-      </main>
-    </>
-  )
+                <form className="flex item-center justify-center mt-10">        
+                <input
+                    type="text"
+                    name="text"
+                    placeholder="Enter Your To do item"
+                    className="py-2 px-5 rounded-lg bg-gray-700 text-white tracking-wide"
+                    
+
+                 />        
+                </form>  
+            </main> 
+        </>
+    )
 }
 
 export default App;
