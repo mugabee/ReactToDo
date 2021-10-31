1381c9219affd26f4ff0bce4029884c87c201457
mugabe richard <mugabe.richard.37@gmail.com>
2021-10-31 05:57:33 +0200

    Creating the useState

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import { useState } from "react"
 import Logo from "./components/Logo"
 import Hero from "./components/Hero"
+import List from "./components/List"
 
 const App = () => {
+    //to catch and insert the text
     const [text, setText] = useState("")
     const [items, setItems] = useState([])
+
+    // to remove the loading event after text input
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
     return ( 
         <>       
             <main>        
                 <Logo />        
                 <Hero />  
 
-                <form className="flex item-center justify-center mt-10">        
+                <form className="flex item-center justify-center mt-10"
+                onSubmit={handleSubmit}>        
                 <input
                     type="text"
                     name="text"
                     placeholder="Enter Your To do item"
                     className="py-2 px-5 rounded-lg bg-gray-700 text-white tracking-wide"
                     value={text}
+                    onChange={(e) => setText(e.target.value)}
                     
 
                  />        
                 </form>  
+                <List />
             </main> 
         </>
     )
