d404581686fa3c6b1bd7395f2b090b7ac313f1c8
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-02 08:37:42 +0200

    project refactor

--- src/App.js
+++ src/App.js
@@ -49,7 +49,8 @@ const App = () => {
         <> 
             
             <main className="lg:pb-96 lg:pt-10 bg-gradient-to-r from-purple-400 via-blue-700 to-indigo-400">
-                <div className="lg:mx-64 border rounded-2xl border-red-100 bg-black bg-opacity-700 ">  
+                <div className="lg:mx-64 border rounded-2xl border-red-100 bg-black bg-opacity-700 "> 
+                
                    
                     <Title />         
 
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,34 @@
+import ReactTypingEffect from "react-typing-effect"
 const Title = () => {
     return (
         <>
-        <header className="flex items-center justify-center">
-            <h1 className="text-2xl text-white mt-10 font-bold uppercase ">To Do</h1>
+        <header className=" ">
+        <h1 className="text-3xl  text-center  text-blue-500 mt-10 font-bold uppercase ">itinerary tracker </h1>
+
+            <h1 className="text-center text-sm text-green-500 mt-2 font-light">Listen better. Plan better. Build better. </h1>
+   
+            <h1 className=" ml-40 text-2xl text-green-500 mt-10 font-bold ">Here's Your <ReactTypingEffect
+        text={["Daily Order", "Things To Be Done", "ToDo List"]}
+        speed={100}
+        eraseSpeed={100}
+        typingDelay={2500}
+        cursorRenderer={cursor => <h1>{cursor}</h1>}
+        displayTextRenderer={(text, i) => {
+          return (
+            <h1 className="text-white">
+              {text.split('').map((char, i) => {
+                const key = `${i}`;
+                return (
+                  <span
+                    key={key}
+                    
+                  >{char}</span>
+                );
+              })}
+            </h1>
+          );
+        }}        
+      /> </h1>
         </header>
             
         </>
