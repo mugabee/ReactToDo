aff2ff2bbb356d40aad1f54a7043ae085e6d09f8
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-08 11:51:28 +0200

    creating pages using react routers

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,6 +32,7 @@ export default class Navbar extends Component {
       <nav className="w-full z-30 top-0 text-black py-1 lg:py-6">
         <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-2 lg:py-6">
           <div className="pl-4 flex items-center">
+              
             <a
               className="text-black no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
               href="#"
