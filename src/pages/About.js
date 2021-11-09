a7ce26fc57029d80886292a729fb65a19930ab04
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-09 09:06:13 +0200

    Project Final Commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,32 +16,3 @@ const About = (props) => {
 export default About
 
 
-// import React, { Component } from 'react'
-// import AddIlinerary from "../components/AddItinerary"
-
-// const handleSubmit = (e) => {
-//     e.preventDefault()
-
-//     const newItems ={
-//         id: uuidv4(),
-//         title: text,
-//         completed: false,
-//     }
-//     //Using the spread operator for the old items to  remain while adding the new ones
-//     setItems([newItems, ...items])
-//     setText("")
-// }
-
-// export default class About extends Component {
-//     render() {
-//         return (
-            
-
-//             <form className="flex item-center justify-center mt-10"
-//             onSubmit={this.props.handleSubmit}>  
-//             <AddIlinerary setText={this.props.setText} text={this.props.text}/>        <Button />
-
-//             </form>  
-//         )
-//     }
-// }
