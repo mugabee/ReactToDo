ac1c660ca94e5ec53463405cd1e6b24c95351cf0
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-09 22:56:41 +0200

    Final Project Commits

--- src/context/ProviderToDo.js
+++ src/context/ProviderToDo.js
@@ -1,59 +1,57 @@
 import { useState } from "react";
 import { ToDoContext } from "./ToDoContext";
-import {v4 as uuidv4} from "uuid"
-
+import { v4 as uuidv4 } from "uuid";
 
 const ProviderToDo = (props) => {
-    const getLocalStorage = () => {
-        let items = localStorage.getItem("items")
-        console.log(items);
-    
-        if(items){
-            return JSON.parse(localStorage.getItem("items"))
-        } else {
-            return []
-        }
+  const getLocalStorage = () => {
+    let items = localStorage.getItem("items");
+    console.log(items);
+
+    if (items) {
+      return JSON.parse(localStorage.getItem("items"));
+    } else {
+      return [];
     }
-    
-     const [editItems, setEditItems] = useState(null)
-        const [text, setText] = useState("")
-        const [dateText, setDate] = useState("")
-        const [timeText, setTime] = useState("")
-        const [items, setItems] = useState(getLocalStorage())
-        console.log(items);
+  };
 
-        const handleSubmit = (e) => {
-            e.preventDefault()
-    
-            const newItems ={
-                id: uuidv4(),
-                title: text,
-                dates: dateText,
-                timeSet: timeText,
-                completed: false,
-            }
-            //Using the spread operator for the old items to  remain while adding the new ones
-            setItems([newItems, ...items])
-            setText("")
-            setDate("")
-            setTime("")
-  
-        }
+  const [editItems, setEditItems] = useState(null);
+  const [text, setText] = useState("");
+  const [dateText, setDate] = useState("");
+  const [timeText, setTime] = useState("");
+  const [items, setItems] = useState(getLocalStorage());
+  console.log(items);
 
-        const itemComplete = (id) =>{
-            console.log(id)
-            setItems(
-                items.map((singleItem) => 
-                    singleItem.id === id ? {...singleItem, completed: !singleItem.completed}
-                    : singleItem      
-                )
-            )
-        }
-    
-        const deleteItem = (id) => {
-            setItems(items.filter((item)=> item.id !==id))
-        }
-       
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newItems = {
+      id: uuidv4(),
+      title: text,
+      dates: dateText,
+      timeSet: timeText,
+      completed: false,
+    };
+    //Using the spread operator for the old items to  remain while adding the new ones
+    setItems([newItems, ...items]);
+    setText("");
+    setDate("");
+    setTime("");
+  };
+
+  const itemComplete = (id) => {
+    console.log(id);
+    setItems(
+      items.map((singleItem) =>
+        singleItem.id === id
+          ? { ...singleItem, completed: !singleItem.completed }
+          : singleItem
+      )
+    );
+  };
+
+  const deleteItem = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
 
   const value = {
     items,
@@ -62,18 +60,16 @@ const ProviderToDo = (props) => {
     handleSubmit,
     getLocalStorage,
     setItems,
-    text, 
+    text,
     setText,
     dateText,
     setDate,
     timeText,
-    setTime
+    setTime,
   };
-return (
+  return (
     <ToDoContext.Provider value={value}>{props.children}</ToDoContext.Provider>
-
-
-);
+  );
 };
 
 export default ProviderToDo;
