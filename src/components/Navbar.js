af762d554c5baf90d26554cf12d0ba3a4487f0dc
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-09 21:57:07 +0200

    add user input date functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,131 +1,137 @@
-import { useState, useEffect } from "react"
-import {v4 as uuidv4} from "uuid"
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Add from "../pages/Add";
 
-
+import ProviderToDo from "../context/ProviderToDo";
 import MainSection from "./MainSection";
 import Footer from "./Footer";
 import Title from "./Title";
 
-
 const Navbar = () => {
-    const getLocalStorage = () => {
-        let items = localStorage.getItem("items")
-        console.log(items);
-    
-        if(items){
-            return JSON.parse(localStorage.getItem("items"))
-        } else {
-            return []
-        }
+  const getLocalStorage = () => {
+    let items = localStorage.getItem("items");
+    console.log(items);
+
+    if (items) {
+      return JSON.parse(localStorage.getItem("items"));
+    } else {
+      return [];
     }
-    
-     const [editItems, setEditItems] = useState(null)
-        const [text, setText] = useState("")
-        const [dateText, setDate] = useState("")
-        const [timeText, setTime] = useState("")
-        const [items, setItems] = useState(getLocalStorage())
-        console.log(items);
+  };
 
-        const handleSubmit = (e) => {
-            e.preventDefault()
-    
-            const newItems ={
-                id: uuidv4(),
-                title: text,
-                dates: dateText,
-                timeSet: timeText,
-                completed: false,
-            }
-            //Using the spread operator for the old items to  remain while adding the new ones
-            setItems([newItems, ...items])
-            setText("")
-            setDate("")
-            setTime("")
-  
-        }
+  const [editItems, setEditItems] = useState(null);
+  const [text, setText] = useState("");
+  const [dateText, setDate] = useState("");
+  const [timeText, setTime] = useState("");
+  const [items, setItems] = useState(getLocalStorage());
+  console.log(items);
 
-        const itemComplete = (id) =>{
-            console.log(id)
-            setItems(
-                items.map((singleItem) => 
-                    singleItem.id === id ? {...singleItem, completed: !singleItem.completed}
-                    : singleItem      
-                )
-            )
-        }
-    
-        const deleteItem = (id) => {
-            setItems(items.filter((item)=> item.id !==id))
-        }
-        useEffect(() => {
-            localStorage.setItem("items", JSON.stringify(items))
-        }, [items]
-        )
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newItems = {
+      id: uuidv4(),
+      title: text,
+      dates: dateText,
+      timeSet: timeText,
+      completed: false,
+    };
+    //Using the spread operator for the old items to  remain while adding the new ones
+    setItems([newItems, ...items]);
+    setText("");
+    setDate("");
+    setTime("");
+  };
+
+  const itemComplete = (id) => {
+    console.log(id);
+    setItems(
+      items.map((singleItem) =>
+        singleItem.id === id
+          ? { ...singleItem, completed: !singleItem.completed }
+          : singleItem
+      )
+    );
+  };
+
+  const deleteItem = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
   return (
     <div>
-      <Router>
-        <nav className="w-full z-30 top-0 text-black py-1 lg:py-6">
-          <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-2 lg:py-6">
-            <div className="pl-4 flex items-center">
-              <Link
-                to="/"
-                className="text-black no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-              >
-                React Work
-              </Link>
+      <ProviderToDo>
+        <Router>
+          <nav className="w-full z-30 top-0 text-black py-1 lg:py-6">
+            <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-2 lg:py-6">
+              <div className="pl-4 flex items-center">
+                <Link
+                  to="/"
+                  className="text-black no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+                >
+                  React Work
+                </Link>
+              </div>
+
+              <div className="" id="nav-content">
+                <ul className="list-reset lg:flex justify-end flex-1 items-center">
+                  <li className="mr-3">
+                    <Link
+                      to="/"
+                      className="inline-block md:border border-black hover:bg-blue-200 text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                    >
+                      Home
+                    </Link>{" "}
+                  </li>{" "}
+                  <li className="mr-3">
+                    {" "}
+                    <Link
+                      to="/Add"
+                      className="inline-block md:border border-black hover:bg-blue-200 text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                    >
+                      Add
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
-  
-            <div className="" id="nav-content">
-              
-              <ul className="list-reset lg:flex justify-end flex-1 items-center">
-                <li className="mr-3">
-                  <Link
-                    to="/"
-                    className="inline-block md:border border-black hover:bg-blue-200 text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                  >
-                    Home
-                  </Link>{" "}
-                </li>{" "}
-                <li className="mr-3">
-                  {" "}
-                  <Link
-                    to="/Add"
-                    className="inline-block md:border border-black hover:bg-blue-200 text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                  >
-                    Add
-                  </Link>
-                </li>                
-              </ul>
-            </div>
+          </nav>
+          <div className="lg:mx-64 border rounded-2xl border-red-100 bg-black bg-opacity-700 ">
+            <Title />
+
+            <Switch>
+              <Route path="/Add">
+                <Add
+                  setText={setText}
+                  setDate={setDate}
+                  setTime={setTime}
+                  timeText={timeText}
+                  dateText={dateText}
+                  handleSubmit={handleSubmit}
+                  text={text}
+                />
+              </Route>
+
+              <Route path="/">
+                <MainSection
+                  items={items}
+                  setItems={setItems}
+                  deleteItem={deleteItem}
+                  setEditItems={setEditItems}
+                  itemComplete={itemComplete}
+                />
+              </Route>
+            </Switch>
+            <Footer />
           </div>
-        </nav>
-        <div className="lg:mx-64 border rounded-2xl border-red-100 bg-black bg-opacity-700 "> 
-                
-                   
-                <Title /> 
-
-          
-        <Switch>
-
-          <Route path="/Add">
-            <Add setText={setText} setDate={setDate} setTime={setTime} timeText={timeText} dateText={dateText} handleSubmit={handleSubmit} text={text} />
-          </Route>
-         
-          <Route path="/">
-                <MainSection items={items} setItems={setItems} deleteItem={deleteItem} setEditItems={setEditItems} itemComplete={itemComplete}/>
-            
-      
-        </Route>
-        </Switch>
-        <Footer />
-            </div> 
-      </Router>
+        </Router>
+      </ProviderToDo>
     </div>
   );
 };
 
 export default Navbar;
-
---
+++ src/context/ProviderToDo.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { ToDoContext } from "./ToDoContext";
+import {v4 as uuidv4} from "uuid"
+
+
+const ProviderToDo = (props) => {
+    const getLocalStorage = () => {
+        let items = localStorage.getItem("items")
+        console.log(items);
+    
+        if(items){
+            return JSON.parse(localStorage.getItem("items"))
+        } else {
+            return []
+        }
+    }
+    
+     const [editItems, setEditItems] = useState(null)
+        const [text, setText] = useState("")
+        const [dateText, setDate] = useState("")
+        const [timeText, setTime] = useState("")
+        const [items, setItems] = useState(getLocalStorage())
+        console.log(items);
+
+        const handleSubmit = (e) => {
+            e.preventDefault()
+    
+            const newItems ={
+                id: uuidv4(),
+                title: text,
+                dates: dateText,
+                timeSet: timeText,
+                completed: false,
+            }
+            //Using the spread operator for the old items to  remain while adding the new ones
+            setItems([newItems, ...items])
+            setText("")
+            setDate("")
+            setTime("")
+  
+        }
+
+        const itemComplete = (id) =>{
+            console.log(id)
+            setItems(
+                items.map((singleItem) => 
+                    singleItem.id === id ? {...singleItem, completed: !singleItem.completed}
+                    : singleItem      
+                )
+            )
+        }
+    
+        const deleteItem = (id) => {
+            setItems(items.filter((item)=> item.id !==id))
+        }
+        // //useEffect(() => {
+        //     localStorage.setItem("items", JSON.stringify(items))
+        // }, [items]
+        // )
+
+  const value = {
+    items,
+    itemComplete,
+    deleteItem,
+    handleSubmit,
+    getLocalStorage
+  };
+return (
+    <ToDoContext.Provider value={value}>{props.children}</ToDoContext.Provider>
+
+
+);
+};
+
+export default ProviderToDo;
