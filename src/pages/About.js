7f32379d81db49b1523b6d3a1597e09ea30e2851
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-09 16:55:23 +0200

    adding the add green Button

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,13 @@
 import AddIlinerary from "../components/AddItinerary"
+import Button from "../components/Button"
 
 const About = (props) => {
     return (
         <div>
              <form className="flex item-center justify-center mt-10"
             onSubmit={props.handleSubmit}>  
-             <AddIlinerary setText={props.setText} handleSubmit={props.handleSubmit} text={props.text}/>   
+             <AddIlinerary setText={props.setText} handleSubmit={props.handleSubmit} text={props.text}/>  
+             <Button /> 
 
              </form>  
             
